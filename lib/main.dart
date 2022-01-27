9ba5e212a22812fa0e1998c66ba7f1ca5afb4357
wonhee <eejoy1212@gmail.com>
2022-01-27 22:50:38 +0900

    [22-01-27]클래스 다만듦

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_provider/pages/todo_page.dart';
+import 'package:to_do_provider/providers/active_todo_count.dart';
+import 'package:to_do_provider/providers/filtered_todos.dart';
+import 'package:to_do_provider/providers/todo_filter.dart';
+import 'package:to_do_provider/providers/todo_list.dart';
+import 'package:to_do_provider/providers/todo_search.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +17,42 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TODOS',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoFilter>(
+          create: (context) => TodoFilter(),
+        ),
+        ChangeNotifierProvider<TodoSearch>(
+          create: (context) => TodoSearch(),
+        ),
+        ChangeNotifierProvider<TodoList>(
+          create: (context) => TodoList(),
+        ),
+        ChangeNotifierProxyProvider<TodoList, ActiveTodoCount>(
+          create: (context) => ActiveTodoCount(),
+          update: (
+            BuildContext context,
+            TodoList todoList,
+            ActiveTodoCount? activeTodoCount,
+          ) =>
+              activeTodoCount!..update(todoList),
+        ),
+        ChangeNotifierProxyProvider3<TodoFilter, TodoSearch, TodoList,
+            FilteredTodos>(
+          create: (context) => FilteredTodos(),
+          update: (context, TodoFilter todoFilter, TodoSearch todoSearch,
+                  TodoList todoList, FilteredTodos? filteredTodos) =>
+              filteredTodos!..update(todoFilter, todoSearch, todoList),
+        )
+      ],
+      child: MaterialApp(
+        title: 'TODOS',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const TodosPage(),
       ),
-      home: const TodosPage(),
     );
   }
 }
--- lib/providers/filtered_todos.dart
+++ lib/providers/filtered_todos.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:to_do_provider/models/todo_model.dart';
+import 'package:to_do_provider/providers/todo_filter.dart';
+import 'package:to_do_provider/providers/todo_list.dart';
+import 'package:to_do_provider/providers/todo_search.dart';
 
 class FilteredTodosState {
   final List<Todo> filteredTodos;
@@ -30,5 +33,30 @@ class FilteredTodosState {
 class FilteredTodos with ChangeNotifier {
   FilteredTodosState _state = FilteredTodosState.initial();
   FilteredTodosState get state => _state;
-  
+
+  void update(TodoFilter todoFilter, TodoSearch todoSearch, TodoList todoList) {
+    List<Todo> _filteredTodos;
+    switch (todoFilter.state.filter) {
+      case Filter.active:
+        _filteredTodos =
+            todoList.state.todos.where((Todo todo) => !todo.completed).toList();
+        break;
+      case Filter.completed:
+        _filteredTodos =
+            todoList.state.todos.where((Todo todo) => todo.completed).toList();
+        break;
+      case Filter.all:
+      default:
+        _filteredTodos = todoList.state.todos;
+        break;
+    }
+    if (todoSearch.state.searchTerm.isNotEmpty) {
+      _filteredTodos = _filteredTodos
+          .where((Todo todo) =>
+              todo.desc.toLowerCase().contains(todoSearch.state.searchTerm))
+          .toList();
+    }
+    _state = _state.copyWith(filteredTodos: _filteredTodos);
+    notifyListeners();
+  }
 }
